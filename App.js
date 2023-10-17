b2d09772ec0e03b6d8f1bf66470379665b2d2e43
Smeet Agrawal <smeet@skillr.ai>
2023-10-18 01:03:14 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { id: "heading", abc: "xyz" }, "This is h1 tag"),
+//     React.createElement("h2", { id: "heading", abc: "xyz" }, "This is h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", { id: "heading", abc: "xyz" }, "This is h1 tag"),
+//     React.createElement("h2", { id: "heading", abc: "xyz" }, "This is h2 tag"),
+//   ]),
+// ]);
+
+// console.log("parent", parent);
+
+// setTimeout(() => {
+//   root.render(parent);
+// }, 5000);
